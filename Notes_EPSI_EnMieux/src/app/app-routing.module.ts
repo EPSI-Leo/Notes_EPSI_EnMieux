2d64046c600f95381a42003791b45bc1c48763ce
Alexis <alexiscontamin@gmail.com>
2024-02-01 16:50:43 +0100

    ajout page login

--- Notes_EPSI_EnMieux/src/app/app-routing.module.ts
+++ Notes_EPSI_EnMieux/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './route-views/login/login.component';
+import { CoursComponent } from './route-views/cours/cours.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'cours', component: CoursComponent},
+  { path: '**', redirectTo: ''},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Notes_EPSI_EnMieux/src/app/app.module.ts
+++ Notes_EPSI_EnMieux/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './route-views/login/login.component';
+import { CoursComponent } from './route-views/cours/cours.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CoursComponent
   ],
   imports: [
     BrowserModule,
