32c7cb009cab9cd369c0e0d872ce72054a5aba62
Alexis <alexiscontamin@gmail.com>
2024-02-02 09:36:09 +0100

    modif api

--- API/Notes_ESPI_en_mieux/ModelViews/CoursClasses.cs
+++ API/Notes_ESPI_en_mieux/ModelViews/CoursClasses.cs
@@ -6,7 +6,7 @@ namespace Notes_ESPI_en_mieux.Entities;
 public partial class CoursClasses
 {
     public int Id { get; set; }
-    public int id_cours { get; set; }
-    public int id_classe { get; set; }
+    public int IdCours { get; set; }
+    public int IdClasse { get; set; }
 
 }
